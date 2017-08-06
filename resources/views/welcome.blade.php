b93b19937ef232ab10a47fdd274edf7b359a98c0
Vitor S Rodrigues <vitor.rodrigues@gmail.com>
2017-08-06 12:03:47 -0300

    Concatenating style-sheets and javascripts with laravel mix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,16 +11,51 @@
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('/css/font-awesome.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('/css/style.css') }}">
     </head>
     <body>
-        <i class="fa fa-address-book" aria-hidden="true"></i>
+        <div class="container">
+            <div class="row">
+                <nav>
+                    <ul>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Contact</li>
+                    </ul>
+                </nav>
+            </div>
 
-        <script type="text/javascript" src="{{ asset('/js/jquery.min.js') }}"></script>
+            <div class="row">
+                <h1>Font-Awesome</h1>
+                <i class="fa fa-address-book" aria-hidden="true"></i>
+            </div>
+
+            <div class="row">
+                <h1>Bootstrap CSS</h1>
+                <span class="glyphicon glyphicon-trash"></span>
+
+                <div class="alert alert-warning" role="alert">
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">x</span>
+                    </button>
+                    <strong>Holy guacamole!</strong> Best check yo self, you're not loocking too good.
+                </div>
+            </div>
+
+            <div class="row">
+                <footer>
+                    Rodapé
+                </footer>
+            </div>
+        </div>
+
+        <script type="text/javascript" src="{{ asset('/js/scripts.js') }}"></script>
+        <!-- Comentado - Transportado para app.js
         <script type="text/javascript">
             $(document).ready(function (){
                 alert('Olá Mundo');
             });
         </script>
+        -->
     </body>
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -16,7 +16,26 @@ let mix = require('laravel-mix');
        .sass('resources/assets/sass/app.scss', 'public/css');
  */
 
-mix.copy('node_modules/font-awesome/fonts', 'public/fonts')
-   .copy('node_modules/font-awesome/css/font-awesome.min.css', 'public/css/font-awesome.min.css');
+/*
+    mix.copy('node_modules/font-awesome/fonts', 'public/fonts')
+       .copy('node_modules/font-awesome/css/font-awesome.min.css', 'public/css/font-awesome.min.css');
 
-mix.copy('node_modules/jquery/dist/jquery.min.js','public/js/jquery.min.js');
+    mix.copy('node_modules/jquery/dist/jquery.min.js','public/js/jquery.min.js');
+ */
+
+mix.copy('node_modules/font-awesome/fonts', 'public/fonts')
+   .copy('node_modules/bootstrap/dist/fonts', 'public/fonts');
+
+mix.styles([
+    'node_modules/font-awesome/css/font-awesome.min.css',
+    'node_modules/bootstrap/dist/css/bootstrap.min.css',
+    'resources/assets/css/app.css',
+    'resources/assets/css/header.css',
+    'resources/assets/css/footer.css'
+],  'public/css/style.css')
+
+mix.scripts([
+    'node_modules/jquery/dist/jquery.min.js',
+    'node_modules/bootstrap/dist/js/bootstrap.min.js',
+    'resources/assets/javascripts/app.js'
+],  'public/js/scripts.js')
