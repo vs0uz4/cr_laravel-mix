b3fc5badd2c9f99ccc5a492cf70f5f41da585897
Vitor S Rodrigues <vitor.rodrigues@gmail.com>
2017-08-06 18:58:48 -0300

    Working Sass in Laravel Mix. Compiling Sass files and generate stylesheets.

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.less('resources/assets/less/app.less', 'public/css');
+mix.sass('resources/assets/sass/app.scss', 'public/css');
 
 mix.scripts([
     'node_modules/jquery/dist/jquery.min.js',
